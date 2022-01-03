e123db03999c1e8dc5a97d80589dba0818d99bd2
Kori Francis <kfrancis@clinicalsupportsystems.com>
2022-01-03 18:11:59 -0500

    stub form page

---
+++ src/RatTracker.Web/Pages/Results/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RatTracker.EntityFrameworkCore;
+using RatTracker.Results;
+
+namespace RatTracker.Web.Pages.Results
+{
+    public class CreateModel : RatTrackerPageModel
+    {
+        [BindProperty]
+        public ResultCreateDto Result { get; set; }
+
+        private readonly IResultsAppService _resultsAppService;
+
+        public CreateModel(IResultsAppService resultsAppService)
+        {
+            _resultsAppService = resultsAppService;
+        }
+
+        public IActionResult OnGet()
+        {
+            Result = new ResultCreateDto();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _resultsAppService.CreateAsync(Result);
+            return NoContent();
+        }
+    }
+}
---
+++ src/RatTracker.Web/Pages/Results/create.js
@@ -0,0 +1,6 @@
+$(function () {
+    var l = abp.localization.getResource("RatTracker");
+    var resultService = window.ratTracker.results.results;
+
+   
+});
