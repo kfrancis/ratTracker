755f038f399a13e51389a7a5a6fcf851cb376d3d
Kori Francis <kfrancis@clinicalsupportsystems.com>
2022-01-03 19:42:20 -0500

    submit form wip

--- src/RatTracker.Web/Pages/Results/create.js
+++ src/RatTracker.Web/Pages/Results/create.js
@@ -2,5 +2,19 @@ $(function () {
     var l = abp.localization.getResource("RatTracker");
     var resultService = window.ratTracker.results.results;
 
-   
+    var _lookupModal = new abp.ModalManager({
+        viewUrl: abp.appPath + "Shared/LookupModal",
+        scriptUrl: "/Pages/Shared/lookupModal.js",
+        modalClass: "navigationPropertyLookup"
+    });
+
+    $('.lookupCleanButton').on('click', '', function () {
+        $(this).parent().parent().find('input').val('');
+    });
+
+    _lookupModal.onClose(function () {
+        var modal = $(_lookupModal.getModal());
+        $('#' + lastNpIdId).val(modal.find('#CurrentLookupId').val());
+        $('#' + lastNpDisplayNameId).val(modal.find('#CurrentLookupDisplayName').val());
+    });
 });
